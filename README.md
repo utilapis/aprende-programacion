d10e911351b6553e3bd8bd75eee3f72cb99ae921
Rodrigo Ibanez <rsibanez89@gmail.com>
2021-01-18 21:32:35 +1100

    03 - Variables, constantes, y tipos de datos.

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 ## 1 - Crea tu primera aplicación que mustra un mensaje por pantalla. | UtilAPIs
 - Video: https://youtu.be/Igs8YyW8rnw
-- Código: https://github.com/utilapis/webstore/tree/feature/01-CreaTuPrimeraAplicacion
+- Código: https://github.com/utilapis/aprende-programacion/tree/feature/01-CreaTuPrimeraAplicacion
 
 ## 2 - Programá usando un Entorno de Desarrollo Integrado (IDE). | UtilAPIs
 - Video: https://youtu.be/TiEFCMuO8wg
-- Código: https://github.com/utilapis/webstore/tree/feature/02-ProgramaUsandoUnIDE
+- Código: https://github.com/utilapis/aprende-programacion/tree/feature/02-ProgramaUsandoUnIDE
+
+## 3 - Variables, constantes, y tipos de datos. | UtilApis
+- Video: https://youtu.be/wZ4CMvLj4ZA
+- Código: https://github.com/utilapis/aprende-programacion/tree/feature/03-VariablesConstantesYTiposDeDatos
--- app/Program.cs
+++ app/Program.cs
@@ -1,8 +1,29 @@
 ﻿using System;
 
 // C#
-Console.WriteLine("_____________________________________");
-Console.WriteLine("HOLA");
-Console.WriteLine("_____________________________________");
-Console.WriteLine("Utilapis!");
-Console.WriteLine("_____________________________________");
+const float IVA = 21;
+
+// Tienda - Precios
+float precioRemera = 320.0F;
+float precioPantalon = 560.0F;
+
+// Cliente compra
+int cantidadRemeras = 15;
+int cantidadPantalones = 5;
+
+// Subtotales
+float subtotalRemera = precioRemera * cantidadRemeras;
+float subtotalPantalon = precioPantalon * cantidadPantalones;
+float subtotal = subtotalRemera + subtotalPantalon;
+float total = (subtotal * IVA/100) + subtotal;
+
+// Facturacion
+char facturaTipo = 'C';
+Console.WriteLine($"---------------------    Factura tipo: {facturaTipo}    ---------------------");
+Console.WriteLine($"Detalle     ---  Precio  ---  Cantidad   ---   Subtotal");
+Console.WriteLine($"Remeras     ---  {precioRemera}     ---  {cantidadRemeras}          ---   {subtotalRemera}");
+Console.WriteLine($"Pantalones  ---  {precioPantalon}     ---  {cantidadPantalones}          ---   {subtotalPantalon}");
+Console.WriteLine($"-----------------------------------------------------------------");
+Console.WriteLine($"Subtotal: {subtotal}");
+Console.WriteLine($"Iva: {IVA}%");
+Console.WriteLine($"Total: {total}");
